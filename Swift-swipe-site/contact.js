b6cee51abb41b304fc513cb94aaed244554758e7
empty <atharavagarwal11@gmail.com>
2024-06-03 20:39:46 +0530

    =fixed errors

--- Swift-swipe-site/contact.js
+++ Swift-swipe-site/contact.js
@@ -1,24 +1,12 @@
 const Submit = document.getElementById('Submit');
 console.log('running')
 
-Submit.addEventListener('click', function() {
-    event.preventDefault();
-    const fields = document.querySelectorAll('.field')
-    for (let i = 0; i<fields.length;i++){
-       
-        if (fields[i].value == ''){
+Submit.addEventListener('click', function () {
+    if (form.checkValidity()) {
+        event.preventDefault();
+        const fields = document.querySelectorAll('.field')
 
-            alert('Please fill all fields')
-            return
-        }   
+        alert(`Name: ${fields[0].value}, Email: ${fields[1].value}, Message: ${fields[2].value}`)
+
     }
-
-    const regex = /^\S+@\S+\.\S+$/
-    if (!regex.test(fields[1].value)){
-        alert('Please enter a valid email')
-        return
-    }
-
-    alert(`Name: ${fields[0].value}, Email: ${fields[1].value}, Message: ${fields[2].value}`)
-    
 })
